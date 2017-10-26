3bcfa096986448b09ea5cbe9ae27b30c4e149e64
lms-github <lms.githubyusen@gmail.com>
2017-10-26 10:42:11 +0700

    Initial commit

---
+++ Solution1.Module/BusinessObjects/Company.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace Solution1.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    public class Company : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (http://documentation.devexpress.com/#Xaf/CustomDocument3146).
+        private string _code;
+        private string _name;
+        
+        public Company(Session session)
+            : base(session)
+        {
+        }
+
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (http://documentation.devexpress.com/#Xaf/CustomDocument2834).
+        }
+
+        public string Code
+        {
+            get { return _code; }
+            set { SetPropertyValue("Code", ref _code, value); }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetPropertyValue("Name", ref _name, value); }
+        }
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (http://documentation.devexpress.com/#Xaf/CustomDocument2619).
+        //    this.PersistentProperty = "Paid";
+        //}
+    }
+}
---
+++ Solution1.Module/BusinessObjects/FFISSysBaseObject.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.ConditionalAppearance;
+
+namespace Solution1.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [NonPersistent()]
+    [DeferredDeletion(false)]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (http://documentation.devexpress.com/#Xaf/CustomDocument2701).
+    public class FFISSysBaseObject : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (http://documentation.devexpress.com/#Xaf/CustomDocument3146).
+        private string _createBy;
+        private DateTime _createDate;
+        private string _modifyBy;
+        private DateTime _modifyDate;
+        public FFISSysBaseObject(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            CreateBy = SecuritySystem.CurrentUserName;
+            CreateDate = DateTime.Now;
+            // Place your initialization code here (http://documentation.devexpress.com/#Xaf/CustomDocument2834).
+        }
+
+        protected override void OnSaving()
+        {
+            base.OnSaving();
+            ModifiedBy = SecuritySystem.CurrentUserName;
+            ModifiedDate = DateTime.Now;
+            
+        }
+
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        [Appearance("CCS10SysBaseObjectCreatedBy", Enabled = false)]
+        [DevExpress.ExpressApp.DC.XafDisplayNameAttribute("User")]
+        public string CreateBy
+        {
+            get { return _createBy; }
+            set { SetPropertyValue("CreateBy", ref _createBy, value); }
+        }
+
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        [Appearance("CCS10SysBaseObjectCreatedDate", Enabled = false)]
+        [DevExpress.ExpressApp.DC.XafDisplayNameAttribute("Date")]
+        public DateTime CreateDate
+        {
+            get { return _createDate; }
+            set { SetPropertyValue("CreateDate", ref _createDate, value); }
+        }
+
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        [Appearance("CCS10SysBaseObjectModifiedBy", Enabled = false)]
+        [DevExpress.ExpressApp.DC.XafDisplayNameAttribute("User")]
+        public string ModifiedBy
+        {
+            get { return _modifyBy; }
+            set { SetPropertyValue("ModifiedBy", ref _modifyBy, value); }
+        }
+
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        [Appearance("CCS10SysBaseObjectModifiedDate", Enabled = false)]
+        [DevExpress.ExpressApp.DC.XafDisplayNameAttribute("Date")]
+        public DateTime ModifiedDate
+        {
+            get { return _modifyDate; }
+            set { SetPropertyValue("ModifiedDate", ref _modifyDate, value); }
+        }
+
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue("PersistentProperty", ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (http://documentation.devexpress.com/#Xaf/CustomDocument2619).
+        //    this.PersistentProperty = "Paid";
+        //}
+    }
+}
